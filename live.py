25fe2d53c52df34bca3a1e503c7e5b9c11b6d139
Eetu Narhi <eetu@eetu.io>
2025-03-08 18:26:46 +0200

    chunk -> block

--- live.py
+++ live.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import threading
 
-CHUNK = 1024
+BLOCK_LENGTH = 1024
 fs = 44100
 
 def input_loop(player):
@@ -20,8 +20,8 @@ def output_loop(out, data, player):
     n = 0
     while n < len(data):
         if player.play:
-            out.write(data[n:n+CHUNK])
-            n += CHUNK
+            out.write(data[n:n+BLOCK_LENGTH])
+            n += BLOCK_LENGTH
         if player.done:
             break
 
@@ -45,7 +45,7 @@ length = 10
 samples = np.linspace(0, length, int(fs*length))
 signal = 0.5 * np.sin(2 * np.pi * 1000 * samples)
 
-out = AudioOutput(fs, CHUNK, 1)
+out = AudioOutput(fs, BLOCK_LENGTH, 1)
 
 t2 = threading.Thread(target=output_loop, args=(out, signal, player))
 t2.start()
