f5c7ec43ca2ffeaf5c19ae0936940888cd8dc60d
Eetu Narhi <eetu@eetu.io>
2025-03-16 15:25:33 +0200

    Change to sine signal, some minor changes

--- live.py
+++ live.py
@@ -5,14 +5,15 @@ import numpy as np
 import soundfile as sf
 
 FS = 44100
-BS = 1024
+BS = 128
 CHANNELS = 1
 
-#length = 10
-#samples = np.linspace(0, length, int(FS*length))
-#signal = 0.5 * np.sin(2 * np.pi * 1000 * samples)
+length = 10
+samples = np.linspace(0, length, int(FS*length))
+signal = 0.5 * np.sin(2 * np.pi * 1000 * samples)
+fs = FS
 
-signal, fs = sf.read("makso.wav")
+#signal, fs = sf.read("makso.wav")
 
 if len(signal.shape) > 1:  
     #signal = np.mean(signal, axis=1)
